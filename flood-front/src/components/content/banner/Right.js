b37748cebc430b00ba5beae6f32da7396066c799
vict0rsch <vsch@protonmail.com>
2019-03-25 23:15:09 -0400

    improve address choice

--- flood-front/src/components/content/banner/Right.js
+++ flood-front/src/components/content/banner/Right.js
@@ -1,23 +1,28 @@
 import React, { Component } from 'react';
-import { Col } from 'antd';
-import { Select, Input } from 'antd';
+import { Col, Row } from 'antd';
+import { Input } from 'antd';
 import Script from 'react-load-script';
 import uuid4 from 'uuid/v4';
-
-const Option = Select.Option;
 
 class Right extends Component {
 
     state = {
         data: [],
         value: undefined,
-        loading: false
+        loading: false,
+        sessiontoken: uuid4(),
+        address: null
     }
 
     handleChange = (ev) => {
         const value = ev.target.value;
+        let ad = this.state.address;
+        if (!value) {
+            ad = null;
+        }
         this.setState({
-            value
+            value,
+            address: ad
         })
     }
 
@@ -49,16 +54,20 @@ class Right extends Component {
         // Check if address is valid
         if (address) {
             // Set State
-            console.log(addressObject.formatted_address);
+            console.log({ addressObject });
             this.setState(
                 {
                     value: addressObject.formatted_address,
+                    address: addressObject,
                 }
             );
         }
     }
 
     render() {
+
+        console.log(`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY || ""}&libraries=places&sessiontoken=${this.state.sessiontoken}`);
+
         return <Col
             xs={ 24 }
             sm={ 12 }
@@ -66,23 +75,47 @@ class Right extends Component {
                 fontSize: '3rem',
                 color: "white",
                 padding: 20,
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center'
             } }
         >
             <Script
-                url="https://maps.googleapis.com/maps/api/js?key=AIzaSyAv_0dp_IjbbjOUD8a-aDGAOg_dRMpjiP4&libraries=places"
+                url={ `https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_GOOGLE_PLACES_API_KEY || ""}&libraries=places&sessiontoken=${this.state.sessiontoken}` }
                 onLoad={ this.handleScriptLoad }
             />
-            <Input
-                style={ { width: '75%' } }
-                size="large"
-                id="autocomplete"
-                onChange={ this.handleChange }
-                value={ this.state.value }
-                allowClear
-            ></Input>
+            <Row
+                align="middle"
+                justify="center"
+                type="flex"
+            >
+                <Input
+                    style={ { width: '90%' } }
+                    size="large"
+                    id="autocomplete"
+                    onChange={ this.handleChange }
+                    value={ this.state.value }
+                    allowClear
+                ></Input>
+            </Row>
+
+            <Row
+                align="middle"
+                justify='center'
+                type="flex"
+                style={ { paddingTop: 8, minHeight: 32 } }
+            >
+                {
+                    this.state.address ?
+                        <a
+                            href={ this.state.address.url }
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            style={ { fontSize: '1rem' } }
+                        >
+                            { this.state.address.formatted_address }
+                        </a>
+                        :
+                        ""
+                }
+            </Row>
         </Col>
     }
 
